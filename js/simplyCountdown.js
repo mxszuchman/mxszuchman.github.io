7d78e9651785f2383aaa097778f0c8e61644d689
Max Szuchman <maxszuchman@MacBook-Air-de-Max.local>
2023-01-02 19:26:07 -0300

    New time

--- js/simplyCountdown.js
+++ js/simplyCountdown.js
@@ -115,9 +115,9 @@
      */
     simplyCountdown = function (elt, args) {
         var parameters = extend({
-                year: 2015,
-                month: 6,
-                day: 28,
+                year: 2023,
+                month: 4,
+                day: 1,
                 hours: 0,
                 minutes: 0,
                 seconds: 0,
